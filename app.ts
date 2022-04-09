708a12e05c3367b7a76cab5059abfe5e86857c01
Tony <htmlcaptain@gmail.com>
2022-04-09 21:31:56 +0900

    Decode the header as utf-8

--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-console.info("fuck you");
+import * as https from 'https';
+
+https.get("https://cn.tbc.wowhead.com/item=32235", response =>
+{
+	console.info(Buffer.from(response.headers.location!, 'binary').toString('utf8'));
+})
