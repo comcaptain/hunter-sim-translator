dec0ba926302073db710d5cbe8bb38b3eb5bf0fd
Tony <htmlcaptain@gmail.com>
2022-04-10 22:46:20 +0900

    Get thread title and url

--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
 import * as https from 'https';
+import { JSDOM } from 'jsdom';
+import { Readable } from 'stream';
 import { createGunzip } from 'zlib';
 
 
 https.get("https://nyaa.si/", response =>
 {
-	console.info(response.headers['content-type']);
-	console.info(response.headers['content-encoding']);
-	const gunzip = createGunzip();
-	response.pipe(gunzip);
-	gunzip.on("data", d =>
+	let output: Readable = response;
+	if (response.headers['content-encoding'] === "gzip")
 	{
-		console.info(d.toString())
+		const gunzip = createGunzip();
+		response.pipe(gunzip);
+		output = gunzip;
+	}
+	output.on("data", d =>
+	{
+		parseContent(d.toString())
 	})
 })
+
+function parseContent(html: string)
+{
+	const dom = new JSDOM(html);
+	dom.window.document.querySelectorAll("a[href^=\\/view\\/]:not(.comments)").forEach(a =>
+	{
+		console.info(`${a.getAttribute("href")}\t${a.textContent}`);
+	})
+}
