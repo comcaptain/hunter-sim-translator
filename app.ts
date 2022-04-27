eae7a2d71151f6a9f6ab894c19f45e60ffce8d96
Tony <htmlcaptain@gmail.com>
2022-04-27 19:45:06 +0900

    Rollback to getting gear chinese name

--- app.ts
+++ app.ts
@@ -1,49 +1,6 @@
 import * as https from 'https';
-import { JSDOM } from 'jsdom';
-import { Readable } from 'stream';
-import { createGunzip } from 'zlib';
 
-
-async function fetch(url: string): Promise<Document>
+https.get("https://cn.tbc.wowhead.com/item=32235", response =>
 {
-	return new Promise(resolve =>
-	{
-		https.get(url, response =>
-		{
-			let output: Readable = response;
-			if (response.headers['content-encoding'] === "gzip")
-			{
-				const gunzip = createGunzip();
-				response.pipe(gunzip);
-				output = gunzip;
-			}
-			const chunks: any[] = [];
-			output.on("data", d =>
-			{
-				chunks.push(d);
-			})
-			output.on("end", () =>
-			{
-				resolve(new JSDOM(Buffer.concat(chunks).toString()).window.document);
-			})
-		})
-	})
-}
-
-async function main()
-{
-	const mainURL = 'https://nyaa.si'
-	const mainDocument = await fetch(mainURL + "/");
-	const aNodes = mainDocument.querySelectorAll("a[href^=\\/view\\/]:not(.comments)");
-	console.info(aNodes.length)
-	for (const a of aNodes)
-	{
-		const url = mainURL + a.getAttribute("href");
-		const title = a.textContent;
-		const threadDOM = await fetch(url);
-		const magnetURL = threadDOM.querySelector("a[href^=magnet]")?.getAttribute("href");
-		console.info(`${title}\t${magnetURL}`);
-	}
-}
-
-main();
+	console.info(Buffer.from(response.headers.location!, 'binary').toString('utf8'));
+})
